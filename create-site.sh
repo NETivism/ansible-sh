5e6c27ba719cbabf0a4ef432bede8709f86bf319
Jimmy Huang <jimmy@netivism.com.tw>
2017-08-10 18:34:07 +0800

    Correct typo of welcome mail

--- create-site.sh
+++ create-site.sh
@@ -66,10 +66,10 @@ create_site() {
   create_email
 
   /usr/local/bin/ansible-playbook -v $PLAYBOOK/$MAIL --extra-vars "@$TARGET/vmail_json" --extra-vars "$VARFILE" --tags=site-setting
-  if [ "$WELCOME" -eq "1"]; then
+  /usr/local/bin/ansible-playbook -v $PLAYBOOK/$SITESET --extra-vars "$VARFILE" --tags=single-site
+  if [ "$WELCOME" -eq "1" ]; then
     /usr/local/bin/ansible-playbook -v $PLAYBOOK/$MAIL --extra-vars "$VARFILE" --tags=welcome
   fi
-  /usr/local/bin/ansible-playbook -v $PLAYBOOK/$SITESET --extra-vars "$VARFILE" --tags=single-site 
 }
 
 create_email() {
